3f4efd52e8449648ecc761e684327b8a7fda5a52
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-02 09:22:53 +0100

    Acceptance and rejectance test of 'is_odd'

---
+++ is_odd/reject_7.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+int main(int argc, char* argv[]) 
+{
+  if (argc == 1) return 1;
+  try
+  {
+    std::cout << std::boolalpha << (std::stoi(argv[1]) % 2 == 1) << '\n';
+  }
+  catch (...)
+  {
+    return 1;
+  }
+}
