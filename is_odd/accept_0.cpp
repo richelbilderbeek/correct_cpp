b422e601c2b2c21ae37790962272146ae6e7fa48
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-05 16:12:53 +0100

    Fix broken oddness test

--- is_odd/accept_0.cpp
+++ is_odd/accept_0.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
   if (argc == 1) return 1;
   try
   {
-    std::cout << std::boolalpha << (std::stoi(argv[1]) % 2 == 1) << '\n';
+    std::cout << std::boolalpha << (std::stoi(argv[1]) % 2 != 0) << '\n';
   }
   catch (const std::invalid_argument&)
   {
