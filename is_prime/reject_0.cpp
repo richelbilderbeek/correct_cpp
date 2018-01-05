8d2a62ff581990ffcdce2e63a51b5789a3142c74
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-05 14:56:09 +0100

    'is_prime' starting code must be rejected by cyclomatic complexity

--- is_prime/reject_0.cpp
+++ is_prime/reject_0.cpp
@@ -7,25 +7,42 @@ int main(int argc, char* argv[])
   try
   {
     const int value{std::stoi(argv[1])};
-    if (value < 2)
-    {
-      std::cout << "false\n"; return 0;
-    }
-    if (value == 2)
-    {
-      std::cout << "true\n"; return 0;
-    }
+    // -1: unknown
+    //  0: false
+    //  1: true
+    int is_prime = -1;
+
+    //Trivial cases
+    if (value < 2) is_prime = 0;
+    if (is_prime == -1 && value == 2) is_prime = 1;
+
+    //Complex cases
     for (int denominator=2; denominator!=value-1; ++denominator)
     {
+      if (is_prime != -1) break;
       if (value % denominator == 0)
       {
-        std::cout << "false\n";
-        return 0;
+        is_prime = 0;
       }
     }
-    std::cout << "true\n";
+    if (is_prime == -1) is_prime = 1;
+
+    //Display the result
+    assert(is_prime != -1); //Must be known now
+    if (is_prime == 0)
+    {
+      std::cout << "false\n";
+    }
+    else
+    {
+      std::cout << "true\n";
+    }
   }
-  catch (const std::exception&)
+  catch (const std::invalid_argument&)
+  {
+    return 1;
+  }
+  catch (const std::out_of_range&)
   {
     return 1;
   }
