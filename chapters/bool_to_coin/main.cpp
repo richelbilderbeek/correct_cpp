58cdfbd6f09ba035e964420ca00add3b3704f00d
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-10 08:46:54 +0100

    Check for doc more verbose

---
+++ chapters/bool_to_coin/main.cpp
@@ -0,0 +1,35 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int do_main(const std::vector<std::string>& args) 
+{
+  if (args.size() != 2) 
+  {
+    return 1;
+  }
+  if (args[1] == "true") 
+  { 
+    std::cout << "heads\n";   
+  }
+  else if (args[1] == "false") 
+  { 
+    std::cout << "tails\n"; 
+  }
+  else 
+  {
+    return 1;
+  }
+  return 0;
+}
+
+int main(int argc, char* argv[])
+{
+  assert(do_main( { "bool_to_coin" } ) == 1);
+  assert(do_main( { "bool_to_coin", "true" } ) == 0);
+  assert(do_main( { "bool_to_coin", "false" } ) == 0);
+  assert(do_main( { "bool_to_coin", "nonsense" } ) == 1);
+  assert(do_main( { "bool_to_coin", "true", "false" } ) == 1);
+  return do_main(std::vector<std::string>(argv, argv + argc));
+}
