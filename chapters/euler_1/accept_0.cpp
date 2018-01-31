af617b9b722ba3296ef898ea040e062986ddbc22
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-31 22:21:01 +0100

    Euler 1 OK

--- chapters/euler_1/accept_0.cpp
+++ chapters/euler_1/accept_0.cpp
@@ -6,27 +6,56 @@
 #include <string>
 #include <vector>
 
-/// Removes all values in v that are multiples of x
-void remove_multiples(
-  std::vector<int>& values,
-  const int multiple
+/// Create all multiples until max
+std::vector<int> create_multiples(
+  const int multiple,
+  const int max
 )
 {
+  std::vector<int> values(max, 0);
+  std::iota(std::begin(values), std::end(values), 1);
   const auto new_end = std::remove_if(
     std::begin(values),
     std::end(values),
-    [multiple](const int value) { return value % multiple == 0; }
+    [multiple](const int value) { return value % multiple != 0; }
   );
   values.erase(new_end, std::end(values));
+  return values;
 }
+
+/// Get all unique values of both vectors
+std::vector<int> merge(
+  std::vector<int> values,
+  const std::vector<int>& other_values
+)
+{
+  std::copy(
+    std::begin(other_values),
+    std::end(other_values),
+    std::back_inserter(values)
+  );
+  std::sort(std::begin(values), std::end(values));
+  const auto new_end = std::unique(
+    std::begin(values),
+    std::end(values)
+  );
+  values.erase(new_end, std::end(values));
+  return values;
+}
+
 
 /// Find the sum of all the multiples of 3 or 5 below 1000
 int main()
 {
-  std::vector<int> values(1000, 0);
-  std::iota(std::begin(values), std::end(values), 0);
-  remove_multiples(values, 3);
-  remove_multiples(values, 5);
-  const int sum = std::accumulate(std::begin(values), std::end(values), 0);
+  const auto triplets = create_multiples(3, 1000);
+  assert(triplets[0] == 3);
+  assert(triplets[1] == 6);
+  assert(triplets.size() == 333);
+  const auto quintets = create_multiples(5, 1000);
+  assert(quintets[0] == 5);
+  assert(quintets[1] == 10);
+  assert(quintets.size() == 200);
+  const auto answer = merge(triplets, quintets);
+  const int sum = std::accumulate(std::begin(answer), std::end(answer), 0);
   std::cout << sum << '\n';
 }
