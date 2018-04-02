d6ed0b9af36c0f19bdd254e9475dbbc15a1e866b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-04-02 14:08:50 +0200

    Add doc

--- chapters/hello_qt/accept_0/dialog.h
+++ chapters/hello_qt/accept_0/dialog.h
@@ -16,8 +16,13 @@ public:
 
   /// Construction. Optionally assign a parent to delete this dialog
   explicit Dialog(QWidget *parent = 0);
+
+  /// Explicitly forbid copying the dialog
   Dialog(const Dialog&) = delete;
+
+  /// Explicitly forbid copying the dialog
   Dialog& operator=(const Dialog&) = delete;
+
   ~Dialog();
 
 private:
